b8715ce42ef377d9ee53d63577ccce471fc7dfa1
Amish <amish.gala@gmail.com>
2014-05-18 00:50:50 -0400

    First commit for Maven Wallet yo!

---
+++ src/main/java/amish/MavenWallet/model/Bill.java
@@ -0,0 +1,13 @@
+package amish.MavenWallet.model;
+
+public class Bill {
+	final int value;
+	
+	public Bill(int value) {
+		this.value = value; 
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
